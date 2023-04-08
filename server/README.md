cea21e85f568c40f129f3a75024045a6123844f5
anhndh-vbee <anhndh.vbee@gmail.com>
2023-04-08 09:13:01 +0700

    Fix api add card

---
+++ server/README.md
@@ -0,0 +1,11 @@
+- Lesson
++ Thêm bài học (thêm tên bài học, thêm thẻ thông qua id của thẻ)
++ Lấy danh sách bài học/1 bài học bất kì
++ Xóa bài học (sẽ không xóa các card bên trong)
++ Update bài học (có thể thêm các thẻ thông qua id của thẻ)
+
+- Card
++ Thêm thẻ: nếu khi tạo thẻ mới mà bổ sung cả lesson chứa thẻ thì lesson cũng sẽ tự cập nhật 
++ Xóa thẻ: lesson (nếu chứa thẻ đó) cũng sẽ cập nhật
++ Update thẻ: chỉ được thay đổi tên, hình ảnh
++ Lấy danh sách thẻ/1 thẻ bất kì
--- server/src/controllers/cardController.js
+++ server/src/controllers/cardController.js
@@ -2,13 +2,16 @@ const { Card, Lesson } = require("../models/lesson_card.js")
 
 const addCard = async (req, res) => {
     try {
-        const card = {
-            image: req.file.path,
-            description: req.body.description,
-            lesson: req.body.lesson
-        }
-        const newCard = new Card(card);
+        const image = req.file.path;
+        const description = req.body.description;
+        const lesson = req.body.lesson
+
+        const newCard = new Card({ image, description, lesson });
         const saveCard = await newCard.save();
+        if (lesson) {
+            const _lesson = await Lesson.findById(lesson);
+            await _lesson.updateOne({ $push: { cards: saveCard._id } });
+        }
         res.status(200).json(saveCard);
     } catch (error) {
         res.status(500).json(error)
